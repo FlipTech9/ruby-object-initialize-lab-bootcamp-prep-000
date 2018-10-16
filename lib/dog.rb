718ac670a5c0c70753c4fe57220067a06f08ffbe
Alex S <alex.sunga@fliptech.co>
2018-10-16 08:19:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,17 @@ class Dog
     @name = name 
   end 
   
+  def initialize(breed)
+    @breed = breed
+  end 
+  
+  def breed=(breed)
+    @breed = breed
+  end 
+  
+  def breed
+    @breed
+  end 
   
   
 end 
